ca4466a2caa394755c51c4308bb62257c1c00a3b
Nadezhda <nbykova79@gmail.com>
2022-07-01 23:09:40 +0300

    Запустили программу определяющую по номеру дня недели выходной

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// по введенной цифре определяется день недели и указывается, является ли он выходным
+Console.WriteLine("Введите порядковый номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string DayWeek()
+{
+if (day >= 1 && day <= 7)
+{
+    if (day >= 1 && day <= 5)
+    {
+       return "Это не выходной";
+    }
+    else {
+         return "Это выходной";
+    }
+}   
+else {
+     return "Вы ввыли не правильный порядковый номер ";
+} 
+
+}
+Console.WriteLine(DayWeek());
